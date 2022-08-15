874e794a1c85d9b6dec61bb4d67d10e9834e8c54
William Axl <williamaxlfc11@gmail.com>
2022-08-14 22:18:24 -0500

    Initial Commit

---
+++ Examen iOS SERTI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen iOS SERTI
+//
+//  Created by Axl on 14/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
